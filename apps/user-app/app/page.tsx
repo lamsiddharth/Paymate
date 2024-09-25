c977b8cfa2518350fb908e8a805f519f4d36ce00
siddharth pradhan <siddharthpradhan2004@gmail.com>
2024-09-25 19:06:09 +0530

    starter repo

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import { Button } from "@repo/ui/button";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className="text-xl">yellow</div>
+  );
+}
---
+++ apps/user-app/lib/auth.ts
@@ -0,0 +1,60 @@
+import db from '@repo/db/client'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import bcrypt from 'bcrypt'
+import Credentials from 'next-auth/providers/credentials'
+
+export const authOptions = {
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                phone: { label: 'Phone number', type: 'text', placeholder:"phone no"},
+                password: { label: 'Password', type: 'password'}
+            },
+            async authorize(credentials: any){
+                const hashedPassword = await bcrypt.hash(credentials.password, 10);
+                const existingUser = await db.user.findFirst({
+                    where: {
+                        number: credentials.phone
+                    }
+                })
+                if(existingUser){
+                    const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password);
+                    if (passwordValidation) {
+                        return {
+                            id: existingUser.id.toString(),
+                            name: existingUser.name,
+                            email: existingUser.number
+                        }
+                    }
+                    return null;
+                }
+                try {
+                    const user = await db.user.create({
+                        data: {
+                            number: credentials.phone,
+                            password: hashedPassword
+                        }
+                    });
+                
+                    return {
+                        id: user.id.toString(),
+                        phoneno: user.number
+                    }
+                } catch(e) {
+                    console.error(e);
+                }
+                return null;
+    
+            }
+
+        })
+    ],
+    secret: process.env.JWT_SECRET || "secret",
+    callbacks: {
+        async session({token, session}: any){
+            session.user.id = token.sub
+            return session
+        }
+    }
+}
