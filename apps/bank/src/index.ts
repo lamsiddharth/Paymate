9def26acafc6bd72d723f76eba085e844cc89070
siddharth pradhan <siddharthpradhan2004@gmail.com>
2024-09-27 07:34:35 +0530

    locking next

--- apps/bank/src/index.ts
+++ apps/bank/src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json())
 
-app.post("/hdfcWebhook", async (req, res) => {
+app.post("/hdfcWebhook", async (req:any, res:any) => {
     //TODO: Add zod validation here?
     //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
     const paymentInformation: {
