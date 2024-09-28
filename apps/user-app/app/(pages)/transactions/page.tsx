9d445ab942ce66576192fb64f619647109aac8a7
siddharth pradhan <siddharthpradhan2004@gmail.com>
2024-09-29 00:35:48 +0530

    Add new featurer fix

--- apps/user-app/app/(pages)/transactions/page.tsx
+++ apps/user-app/app/(pages)/transactions/page.tsx
@@ -3,6 +3,8 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
 
+
+
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
