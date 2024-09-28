a2acbbcd135bc990cfe9035961f60d62cdefaeb1
siddharth pradhan <siddharthpradhan2004@gmail.com>
2024-09-29 00:23:06 +0530

    Add new feature or fix

--- apps/user-app/app/(pages)/transactions/page.tsx
+++ apps/user-app/app/(pages)/transactions/page.tsx
@@ -2,6 +2,7 @@ import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
+
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
